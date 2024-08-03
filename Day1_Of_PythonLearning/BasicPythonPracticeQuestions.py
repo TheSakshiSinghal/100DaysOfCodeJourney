a5a0550f0e9b499bfa05c90cba2ed36f9c670e71
SINGHALLSAKSHI <sakshi.professional09@gmail.com>
2024-08-03 11:39:05 +0530

    Day1_of_PythonLearning
    Day1 of learning python #100DaysOfCodeJourney

---
+++ Day1_Of_PythonLearning/BasicPythonPracticeQuestions.py
@@ -0,0 +1,40 @@
+# print first 10 natural numbers using while loop
+num = int(input("Enter Number:"))
+while(num<=10):
+    print(num)
+    num = num + 1
+
+#print the pattern 
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4 
+# 1 2 3 4 5
+
+
+# print number from 1 to 100
+# i = 1
+# while i<=100:
+#     print(i)
+#     i = i+1
+
+
+# print number from 100 to 1
+# i=100
+# while i>0:
+#     print(i)
+#     i-=1
+
+# print the multiplication table of a number n
+# n= int(input("Enter number for multipication:"))
+# i = 1
+# while i<=10:
+#     print(n*i)
+#     i=i+1
+
+#print the elements of the following list using a loop
+# num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# search for a number x in this tuple using loop
+# num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
---
+++ Day1_Of_PythonLearning/list.py
@@ -0,0 +1,23 @@
+#list methods
+
+#pop function
+my_list = [ 1, 4, "Sakshi"]
+my_list.pop()
+print(my_list)
+
+# reserve and sort function in list
+llist = [1, 4, 'Sakshi', 'hello', 'a']
+llist.reverse()
+print(llist)
+
+new_list =[1, 4, 6, 2, 0]
+new_list.sort()
+print(new_list)
+
+#list nesting 
+list1 = [1, 'sakshi', 'june', 5]
+list2= ['yash', 'karauli', 90, 'w']
+list3 = [4, 6, 'disha', 'x']
+Matrix = [list1, list2, list3]
+print(Matrix[2][0])
+
